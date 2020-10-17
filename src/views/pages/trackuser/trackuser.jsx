6fb95a9aa18c605cb65c1dbaeac2ec2ca591ff82
Muhammad Haris <hariszulfiqar054>
2020-10-17 17:09:31 +0500

    commit

---
+++ src/views/pages/trackuser/trackuser.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+require("dotenv").config();
+
+function Trackuser(props) {
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation(position.coords);
+      console.log(position.coords.latitude);
+    });
+  }, []);
+  return (
+    <Map google={props.google} zoom={2}>
+      <Marker
+        onClick={() => console.log("marker")}
+        name={"Current location"}
+        position={{ lat: location?.latitude, lng: location?.longitude }}
+      />
+
+      {/* <InfoWindow onClose={this.onInfoWindowClose}>
+        <div>
+          <h1>{this.state.selectedPlace.name}</h1>
+        </div>
+      </InfoWindow> */}
+    </Map>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBM07T-MkC4QGBrD-qFVpj_2YDrGdhYD1A",
+})(Trackuser);
--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,86 +1,97 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import {
-  CBadge,
   CCard,
   CCardBody,
   CCardHeader,
   CCol,
   CDataTable,
   CRow,
-  CPagination
-} from '@coreui/react'
+  CPagination,
+  CButton,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalFooter,
+  CModalBody,
+} from "@coreui/react";
 
-import usersData from './UsersData'
-
-const getBadge = status => {
-  switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
-  }
-}
+import usersData from "./UsersData";
 
 const Users = () => {
-  const history = useHistory()
-  const queryPage = useLocation().search.match(/page=([0-9]+)/, '')
-  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1)
-  const [page, setPage] = useState(currentPage)
+  const history = useHistory();
+  const queryPage = useLocation().search.match(/page=([0-9]+)/, "");
+  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1);
+  const [page, setPage] = useState(currentPage);
 
-  const pageChange = newPage => {
-    currentPage !== newPage && history.push(`/users?page=${newPage}`)
-  }
+  const pageChange = (newPage) => {
+    currentPage !== newPage && history.push(`/users?page=${newPage}`);
+  };
 
   useEffect(() => {
-    currentPage !== page && setPage(currentPage)
-  }, [currentPage, page])
+    currentPage !== page && setPage(currentPage);
+  }, [currentPage, page]);
 
   return (
     <CRow>
-      <CCol xl={6}>
+      {/* <CModal show={true} onClose={() => console.log("")}>
+        <CModalHeader closeButton>
+          <CModalTitle>Modal title</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="primary">Do Something</CButton>{" "}
+          <CButton color="secondary" onClick={() => console.log('asd')}>
+            Cancel
+          </CButton>
+        </CModalFooter>
+      </CModal> */}
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <CCol col="6" sm="4" md="2" xl='4' className="mb-3  ">
+          <CButton block color="success">
+            Add Salesman
+          </CButton>
+        </CCol>
+      </div>
+      <CCol xl={12}>
         <CCard>
-          <CCardHeader>
-            Users
-            <small className="text-muted"> example</small>
-          </CCardHeader>
+          <CCardHeader>Salesman</CCardHeader>
           <CCardBody>
-          <CDataTable
-            items={usersData}
-            fields={[
-              { key: 'name', _classes: 'font-weight-bold' },
-              'registered', 'role', 'status'
-            ]}
-            hover
-            striped
-            itemsPerPage={5}
-            activePage={page}
-            clickableRows
-            onRowClick={(item) => history.push(`/users/${item.id}`)}
-            scopedSlots = {{
-              'status':
-                (item)=>(
-                  <td>
-                    <CBadge color={getBadge(item.status)}>
-                      {item.status}
-                    </CBadge>
-                  </td>
-                )
-            }}
-          />
-          <CPagination
-            activePage={page}
-            onActivePageChange={pageChange}
-            pages={5}
-            doubleArrows={false} 
-            align="center"
-          />
+            <CDataTable
+              items={usersData}
+              fields={[
+                { key: "id", _classes: "font-weight-bold" },
+                "name",
+                "registered",
+                "role",
+              ]}
+              hover
+              striped
+              itemsPerPage={5}
+              activePage={page}
+              clickableRows
+              onRowClick={(item) => history.push(`/users/${item.id}`)}
+            />
+            <CPagination
+              activePage={page}
+              onActivePageChange={pageChange}
+              pages={5}
+              doubleArrows={false}
+              align="center"
+            />
           </CCardBody>
         </CCard>
       </CCol>
     </CRow>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
